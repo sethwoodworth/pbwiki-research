152c487fa33e1b46990de32595780321e058526e
Seth Woodworth <seth@isforinsects.com>
2010-06-28 17:29:14 -0400

    moving from file_get_contents to curl in proxy

--- html/proxy.php
+++ html/proxy.php
@@ -1,8 +1,22 @@
 <?php
+function file_get_contents_curl($url) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return data
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+
+
 $url = $_GET['url'];
 header('Content-type: application/json');
 
-$response = trim(file_get_contents($url));
+$response = trim(file_get_contents_curl($url));
 $explosion = explode("\n", $response);
 $lastindex = count($explosion) - 1;
 if($explosion[$lastindex] == '*/'){
