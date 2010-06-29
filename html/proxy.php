9a969201bcd40b318e14eb3734ff3c77bc698a0c
Seth Woodworth <seth@isforinsects.com>
2010-06-28 22:03:26 -0400

    proxy now uses curl and turns space to +, (has cookie code)

--- html/proxy.php
+++ html/proxy.php
@@ -1,8 +1,13 @@
 <?php
+
+
 function file_get_contents_curl($url) {
+    // use cURL to fetch instead of file_get_contents. more extensible
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_HEADER, 0);
+    //curl_setopt($ch, CURLOPT_COOKIEJAR, './cookiejar.txt'); // put cookie in jar?
+    //curl_setopt($ch, CURLOPT_COOKIEFILE, './cookiejar.txt');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return data
     curl_setopt($ch, CURLOPT_URL, $url);
 
@@ -14,8 +19,12 @@ function file_get_contents_curl($url) {
 
 
 $url = $_GET['url'];
+// for debugging
+//$url = "http://alexmoye.pbworks.com/api_v2/op/GetPageRevisions/page/Ben+Smith+Day+1/_type/jsontext";
+$url = str_replace(' ', '+', $url);
 header('Content-type: application/json');
 
+// rid ourselves of needless header response
 $response = trim(file_get_contents_curl($url));
 $explosion = explode("\n", $response);
 $lastindex = count($explosion) - 1;
@@ -27,6 +36,8 @@ if($explosion[0] == '/*-secure-'){
 }
 $return = implode("\n", $explosion);
 
+
+// reply with the json result
 echo $return . "\n";
 
 
