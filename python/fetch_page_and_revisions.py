a452f509626866ab505cf4a33da068820d0ff732
Seth Woodworth <seth@isforinsects.com>
2010-07-13 03:56:44 -0400

    code seems to be working upto the revision pull, failing on author (wrap in try)

--- python/fetch_page_and_revisions.py
+++ python/fetch_page_and_revisions.py
@@ -6,77 +6,62 @@ import sqlalchemy as sql
 
 
 
-def save_wiki(url, json_dict):
+def save_rev(url, page, save_dict):
     dict = {}
     dict['url'] = url
-    try:
-        dict['files_count'] = int(json_dict['GetFiles']['_total_count'])
-        dict['files_list'] = str([x['name'] for x in json_dict['GetFiles']['files']])
+    dict['page_title'] = page
 
-        dict['page_char_size'] = str(json_dict['page']['size'])
-        dict['page_html'] = str(json_dict['page']['html'])
-        dict['page_name'] = str(json_dict['page']['name'])
-        dict['page_revision'] = int(json_dict['page']['revcount'])
+    dict['author'] = str(save_dict['author']['name'])
+    dict['author_uid'] = str(save_dict['author']['uid'])
+    dict['html'] = str(save_dict['html'])
+    dict['time'] = int(save_dict['mtime'])
+    dict['hash'] = str(save_dict['oid'])
+    dict['revision'] = int(save_dict['revcount'])
+    dict['revurl'] = str(save_dict['revurl'])
+    dict['char_size'] = str(save_dict['size'])
+    dict['wikistyle'] = str(save_dict['wikistyle'])
 
-        dict['pages_count'] = int(json_dict['GetPages']['_total_count'])
-        dict['pages_list'] = str([x['name'] for x in json_dict['GetPages']['pages']])
-
-        dict['time_pageedit'] = int(json_dict['GetTimes']['pagetime'])
-        dict['time_file'] = int(json_dict['GetTimes']['filetime'])
-        dict['time_tag'] = int(json_dict['GetTimes']['tagtime'])
-        dict['time_permission'] = int(json_dict['GetTimes']['permtime'])
-        dict['time_comment'] = int(json_dict['GetTimes']['commenttime'])
-        dict['times_checked_at'] = int(json_dict['GetTimes']['_valid_as_of'])
-
-        dict['works'] = 1
-    except:
-        dict['works'] = 0
-    
-
-    i = wikis_table.insert()                                                                    
+    i = revs_table.insert()                                                                    
     i.execute(dict)
 
 
-def pb_api_calls(url):
-    wiki_ops = ['GetFiles', 'GetPages', 'GetTimes']
+def pb_api_calls(url, page):
+    print url
     wiki_api = PBWiki(url)
-    json = {}
-    for op in wiki_ops:
-        json[op] = wiki_api.api_call(op)
-    kwarg = {"page" : 'FrontPage'}
-    try:
-        json['page'] = wiki_api.api_call('GetPage', **kwarg)
-    except:
-        print "FrontPage doesn't exist for this wiki"
-        json['page'] = {'page': {'size': 0, 'html': '', 'page': ''}}
-    return json
+    arg = {"page" : page}
+    print arg
+    data_d = {}
+    rev_list = wiki_api.api_call('GetPageRevisions', **arg)['revisions']
+    for rev in rev_list:
+        print rev
+        kwarg = {"revision": rev, "page": page}
+        data_d[rev] = wiki_api.api_call('GetPage', **arg)
+    for save in data_d:
+        save_rev(url, page, save)    
 
-def filter_wikilist(wiki_list):
-    query = "select url from edu_wikis;"
-    done = engine.execute(sql.text(query)).fetchall()
-    [wiki_list.remove(str(x)[3:-3]) for x in done]
-    return wiki_list
+        
 
-def load_wikilist():
-    # return wikis from file as a list
-    f = open('../nocheckin/wiki-list.txt', 'r')
-    wlist = []
-    for row in f:
-        wlist.append(row[:-1])
-    return wlist
+def load_revs(url, page):
+    print page
+    api_return = pb_api_calls(url, page)
 
-def recall_wikis()
-    query = "select url,pages from edu_wikis where have_pages = 0 and works = 1';"
+def recall_wikis():
+    query = "select url,pages_list from edu_wikis where have_pages=0 and works=1;"
     pageless_wikis = engine.execute(sql.text(query)).fetchall()
-    return pageles_wikis
+    return pageless_wikis
 
 def fetch_all(debug = False):
     if debug:
         print "Status:**** Debugmode active****"
-        wikis_list = recall_wikis()
+        wikis_tuples = recall_wikis()
     else:
-        wikis_list = recall_wikis()
-    return wikis_list
+        wikis_tuples = recall_wikis()
+
+    for t in wikis_tuples:
+        url = t[0]
+        pages = eval(t[1])
+        #print url
+        [load_revs(url, page) for page in pages]
 
     #for wiki in wikis_list:
     #    print "Status:     retreving and storing url  " + str(wiki)
