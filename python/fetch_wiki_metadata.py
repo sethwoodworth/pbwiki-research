d5ef562dd4b2694e154160326340c088b2e2d46b
Seth Woodworth <seth@isforinsects.com>
2010-07-13 02:35:46 -0400

    now recovers from errorquit

--- python/fetch_wiki_metadata.py
+++ python/fetch_wiki_metadata.py
@@ -2,32 +2,36 @@
 from pbwiki import *
 from store_wikis import *
 from sqlalchemy import Text
+import sqlalchemy as sql
 
 
 
 def save_wiki(url, json_dict):
     dict = {}
     dict['url'] = url
+    try:
+        dict['files_count'] = int(json_dict['GetFiles']['_total_count'])
+        dict['files_list'] = str([x['name'] for x in json_dict['GetFiles']['files']])
 
-    dict['files_count'] = int(json_dict['GetFiles']['_total_count'])
-    dict['files_list'] = str([x['name'] for x in json_dict['GetFiles']['files']])
+        dict['page_char_size'] = str(json_dict['page']['size'])
+        dict['page_html'] = str(json_dict['page']['html'])
+        dict['page_name'] = str(json_dict['page']['name'])
+        dict['page_revision'] = int(json_dict['page']['revcount'])
 
-    dict['page_char_size'] = str(json_dict['page']['size'])
-    dict['page_html'] = str(json_dict['page']['html'])
-    dict['page_name'] = str(json_dict['page']['name'])
-    dict['page_revision'] = int(json_dict['page']['revcount'])
+        dict['pages_count'] = int(json_dict['GetPages']['_total_count'])
+        dict['pages_list'] = str([x['name'] for x in json_dict['GetPages']['pages']])
 
-    dict['pages_count'] = int(json_dict['GetPages']['_total_count'])
-    dict['pages_list'] = str([x['name'] for x in json_dict['GetPages']['pages']])
+        dict['time_pageedit'] = int(json_dict['GetTimes']['pagetime'])
+        dict['time_file'] = int(json_dict['GetTimes']['filetime'])
+        dict['time_tag'] = int(json_dict['GetTimes']['tagtime'])
+        dict['time_permission'] = int(json_dict['GetTimes']['permtime'])
+        dict['time_comment'] = int(json_dict['GetTimes']['commenttime'])
+        dict['times_checked_at'] = int(json_dict['GetTimes']['_valid_as_of'])
 
-    dict['time_pageedit'] = int(json_dict['GetTimes']['pagetime'])
-    dict['time_file'] = int(json_dict['GetTimes']['filetime'])
-    dict['time_tag'] = int(json_dict['GetTimes']['tagtime'])
-    dict['time_permission'] = int(json_dict['GetTimes']['permtime'])
-    dict['time_comment'] = int(json_dict['GetTimes']['commenttime'])
-    dict['times_checked_at'] = int(json_dict['GetTimes']['_valid_as_of'])
-
-    dict['works'] = 1
+        dict['works'] = 1
+    except:
+        dict['works'] = 0
+    
 
     i = wikis_table.insert()                                                                    
     i.execute(dict)
@@ -40,13 +44,17 @@ def pb_api_calls(url):
     for op in wiki_ops:
         json[op] = wiki_api.api_call(op)
     kwarg = {"page" : 'FrontPage'}
-    json['page'] = wiki_api.api_call('GetPage', **kwarg)
+    try:
+        json['page'] = wiki_api.api_call('GetPage', **kwarg)
+    except:
+        print "FrontPage doesn't exist for this wiki"
+        json['page'] = {'page': {'size': 0, 'html': '', 'page': ''}}
     return json
 
-def sort_wikilist(wiki_list):
+def filter_wikilist(wiki_list):
     query = "select url from edu_wikis;"
     done = engine.execute(sql.text(query)).fetchall()
-    [wiki_list.remove(x) for x in done]
+    [wiki_list.remove(str(x)[3:-3]) for x in done]
     return wiki_list
 
 def load_wikilist():
@@ -54,7 +62,7 @@ def load_wikilist():
     f = open('../nocheckin/wiki-list.txt', 'r')
     wlist = []
     for row in f:
-        wlist.append(row)
+        wlist.append(row[:-1])
     return wlist
 
 def fetch_wiki_info(debug = False):
@@ -63,14 +71,12 @@ def fetch_wiki_info(debug = False):
         wikis_list = ['http://testapi.pbworks.com ', 'http://alexmoye.pbworks.com ']
     else:
         unsorted_list = load_wikilist()
-        wikis_list = sort_wikilist(unsorted_list)
+        wikis_list = filter_wikilist(unsorted_list)
 
     for wiki in wikis_list:
-        print "Status:     retreving and storing url  " + str(wiki)[:-1]
-        api_response = pb_api_calls(wiki[:-1])
+        print "Status:     retreving and storing url  " + str(wiki)
+        api_response = pb_api_calls(wiki)
         save_wiki(wiki, api_response)
 
 if __name__ == '__main__':
-    #fetch_wiki_info(debug = True)
-    wiki_list = load_wikilist()
-    list = sort_wikilist(wiki_list)
+    fetch_wiki_info()
